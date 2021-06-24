98e081d0f8fea7ed115e80771aa29d52485964d8
Evgenii Dudal <wolfstrace@gmail.com>
2021-06-24 18:09:37 +0300

    user repository alpha

--- src/Migrations/m200906_231853_create_user_table.php
+++ src/Migrations/m200906_231853_create_user_table.php
@@ -16,15 +16,16 @@ class m200906_231853_create_user_table extends Migration
     public function safeUp()
     {
         $this->createTable($this->table, [
-            'id' => "BINARY(16) NOT NULL",
+            'uuid' => "BINARY(16) NOT NULL",
             'registration_email' => $this->string()->notNull()->defaultValue(''),
             'registration_phone' => $this->string(24)->notNull()->defaultValue(''),
             'password_hash' => $this->string()->notNull(),
             'registered_on' => $this->timestamp()->notNull(),
             'status' => $this->tinyInteger()->notNull(),
             'auth_key' => $this->string(40)->notNull()->defaultValue(''),
+            'contacts' => $this->json()->notNull()->defaultValue(json_encode([]))
         ], $this->tableOptions);
-        $this->addPrimaryKey('', $this->table, 'id');
+        $this->addPrimaryKey('', $this->table, 'uuid');
     }
 
     /**
--- src/Repositories/Yii2DaoUserRepository.php
+++ src/Repositories/Yii2DaoUserRepository.php
@@ -15,9 +15,13 @@ namespace RobotE13\Yii2UserAccount\Repositories;
 use Yii;
 use yii\db\Connection;
 use samdark\hydrator\Hydrator;
+use RobotE13\DDD\Entities\Uuid\Id;
 use RobotE13\UserAccount\Entities\User;
-use RobotE13\UserAccount\Repositories\UserRepository;
 use RobotE13\Yii2UserAccount\Adapters\ContactsSerializer;
+use RobotE13\UserAccount\Repositories\{
+    UserRepository,
+    NotFoundException
+};
 
 /**
  * Description of Yii2DaoUserRepository
@@ -47,15 +51,27 @@ class Yii2DaoUserRepository implements UserRepository
     public function findByEmail($email): User
     {
 
+        return $this->populate($this->findRow(['registration_email' => $email]));
     }
 
     public function findById($id): User
     {
+        return $this->populate($this->findRow(['uuid' => Id::fromString($id)->getBytes()]));
     }
 
     public function remove($id): void
     {
-
+        $uid = Id::fromString($id)->getBytes();
+        if((new \yii\db\Query())
+                        ->from("{{%user}}")
+                        ->where(['uuid' => $uid])
+                        ->exists())
+        {
+            $this->dbConnection->createCommand()->delete('{{%user}}', ['uuid' => $uid])->execute();
+        } else
+        {
+            throw new NotFoundException('User not found.');
+        }
     }
 
     public function update(User $user): void
@@ -63,17 +79,50 @@ class Yii2DaoUserRepository implements UserRepository
 
     }
 
+    private function findRow($condition): array
+    {
+        $row = (new \yii\db\Query())
+                ->from("{{%user}}")
+                ->where($condition)
+                ->one();
+
+        if(!$row)
+        {
+            throw new NotFoundException('User not found.');
+        }
+
+        return $row;
+    }
+
     private function extract(User $user)
     {
         $hydrator = new Hydrator(['password_hash' => 'hash']);
         return array_merge([
-            'id' => $user->getUid()->getBytes(),
+            'uuid' => $user->getUid()->getBytes(),
             'registration_email' => $user->getRegistrationEmail(),
             'registration_phone' => '',
             'registered_on' => $user->getRegisteredOn()->format('Y-m-d H:i:s'),
             'status' => $user->getStatus()->getValue(),
             'contacts' => json_encode((new ContactsSerializer($user->getContacts()))->jsonSerialize()),
                 ], $hydrator->extract($user->getPassword()));
+    }
+
+    private function populate($row): User
+    {
+        $row['uuid'] = Id::fromBytes($row['uuid']);
+        $row['password_hash'] = (new Hydrator(['password_hash' => 'hash']))->hydrate(['password_hash' => $row['password_hash']], \RobotE13\UserAccount\Entities\Password::class);
+        $row['registered_on'] = new \DateTimeImmutable($row['registered_on']);
+        $row['contacts'] = new \RobotE13\UserAccount\Entities\Contact\ContactsCollection(json_decode($row['contacts'], true));
+        $hydrator = new Hydrator([
+            'uuid' => 'uid',
+            'registration_email' => 'registrationEmail',
+            'password_hash' => 'password',
+            'registered_on' => 'registeredOn',
+            'status' => 'status',
+            'contacts' => 'contacts'
+        ]);
+
+        return $hydrator->hydrate($row, User::class);
     }
 
 }
